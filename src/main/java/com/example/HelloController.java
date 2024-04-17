8eeb9dc5579c44bcde2627fd6ba989d65db96d12
padmavathimudunuri <85559183+padmavathimudunuri@users.noreply.github.com>
2024-04-17 16:07:56 +0100

    Update HelloController.java

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,13 +1,14 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class SpringbootDemoApplication {
+@RestController
+public class HelloController {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootDemoApplication.class, args);
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
     }
 }
 
